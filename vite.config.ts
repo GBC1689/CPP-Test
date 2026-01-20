73da9cfb6c2478c0f234d1cd13f593922d13b4be
lexicon1971 <lexicon.sm@gmail.com>
2026-01-20 07:43:46 +0200

    Final link and config update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/CPP-Test-/',
+  build: {
+    emptyOutDir: true, // This clears the old files before cooking new ones
+    rollupOptions: {
+      input: 'index.html'
+    }
+  }
 });
